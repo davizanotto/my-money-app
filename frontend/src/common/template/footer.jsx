b28dd101800b70defd94d26b64551893b6bd47fd
Davi Zanotto <davizanotto@gmail.com>
2017-06-11 14:48:51 -0300

    Estrutura inicial montada com header, footer, corpo e menu. Falta construir as rotas.

---
+++ frontend/src/common/template/footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default props => (
+    <footer className='main-footer'>
+        <strong>
+            Copyright &copy; 2017
+            <a href='http://buffetchurrascogaucho.com.br' target='_blank'>Nottos</a>.
+        </strong>
+    </footer>
+)
